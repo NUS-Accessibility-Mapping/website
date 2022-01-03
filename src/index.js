f6e5b25774b11b871792020ad3df3f29fa71999e
Hazel1603 <tan.hazel.2ooo@gmail.com>
2022-01-03 23:26:48 +0800

    fix index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <About />
+    <App />
+    {/* <About /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
